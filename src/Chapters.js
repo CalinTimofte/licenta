2d7bf091029fcdee7c75aad383b08ca3c8e30643
Calin Timofte <calin.timofte@yahoo.com>
2021-11-17 14:08:21 +0100

    feature: arrow changes on menu click, bug with arrow needs to be fixed

--- src/Chapters.js
+++ src/Chapters.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Chapters(){
+
+    let [open, reverseArrow] = useState(false)
+
+    let reverseMenu = () => {reverseArrow(() => (!open))}
+
     return(
-        <>
+        <div className = "menu-up">
             <div id = "demo" class = "collapsible">
                 Lorem ipsum dolor text....
             </div>
-            <a href = "#demo" data-bs-toggle="collapse">Collapsible</a>
-        </>
+            <a href = "#demo" data-bs-toggle="collapse" onClick = {reverseMenu}><i className={open? "fa fa-arrow-up" : "fa fa-arrow-down"} aria-hidden="true"></i></a>
+        </div>
     )
 }
 
