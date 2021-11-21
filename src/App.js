2e70a66995c123098f22a525cb26beaf806a8ae6
Calin Timofte <calin.timofte@yahoo.com>
2021-11-21 23:11:14 +0100

    feature: completes main viewing logic

--- src/App.js
+++ src/App.js
@@ -7,36 +7,74 @@ import {LP1Introducere} from './Chapters/LP1/Introducere';
 
 export default function App(){
     let chapters = {
-        lp : {subchapter1: [
+        lp : [
+            {
+                name: "Introducere",
+                content:[
+                    {name:"Intro", content: LPIntroducere}
+                ]
+            },
+            {
+                name: "Subchapter2",
+                content:[
                 {name: "Section1", content: <p>Section1 Text</p>},
                 {name: "Exercises-Section1", content: <div style = {{display: "flex", flexDirection: "column"}}><p>Exercise1</p><p>Exercise2</p></div>}
-            ]},
-        lp1 : {subchapter1: LP1Introducere}
+                ]
+            }
+            ],
+        lp1 : [
+            {
+                name: "Introducere",
+                content:[{name:"Intro", content: LP1Introducere}]
+            }
+        ]
     }
 
     let [activeChapter, changeActiveChapter] = useState("LP");
+
+    let findActiveChapter = () => {
+        switch(activeChapter){
+            case "LP":  return chapters.lp;
+            case "LP1": return chapters.lp1;
+            default: return chapters.lp;
+        }
+    }
 
     let selectActiveChapter = (chapterName)=>{
         changeActiveChapter(chapterName);
     }
 
-    let [activeSubChapter, changeActiveSubchapter] = useState("Section1");
+    let [activeSubChapter, changeActiveSubchapter] = useState("Introducere");
 
-    let findActiveSubchapter = () => (chapters.lp.subchapter1.find(section => section.name === activeSubChapter))
-    let getSubchapterNames = () => (chapters.lp.subchapter1.map(obj => obj.name))
+    let findActiveSubchapter = () => (findActiveChapter().find(subchapter => subchapter.name === activeSubChapter))
+    let getSubchapterNames = () => (findActiveChapter().map(obj => obj.name))
 
     let selectActiveSubchapter = (subchapterName)=>{
         changeActiveSubchapter(subchapterName);
     }
 
+    let [activeSection, changeActiveSection] = useState("Intro");
+
+    let findActiveSection = () => (findActiveSubchapter().content.find(section => section.name === activeSubChapter))
+
+    let selectActiveSection = (sectionName)=>{
+        changeActiveSection(sectionName);
+    }
+
     let displayContent = () => {
-        return(findActiveSubchapter().content)
+        return(findActiveSection().content)
     } 
 
     return(
         <>
             <Chapters chapterHandler = {selectActiveChapter} activeChapter = {activeChapter}/>
-            <SubChapters subchapterNames = {getSubchapterNames()} subchapterHandler = {selectActiveSubchapter} activeSubChapter = {activeSubChapter}/>
+            <SubChapters 
+                subchapterNames = {getSubchapterNames()} 
+                subchapterHandler = {selectActiveSubchapter} 
+                activeSubChapter = {activeSubChapter}
+                sectionHandler = {selectActiveSection}
+                activeSection = {activeSection}
+            />
             <Content content = {displayContent()}/>
         </>
     )
--- src/Subchapters.js
+++ src/Subchapters.js
@@ -1,26 +1,44 @@
 import React, {useState} from 'react';
 import ArrowButton from './Reusables/ArrowButton';
 
-export default function SubChapters({subchapterNames, subchapterHandler, activeSubChapter}){
+export default function SubChapters({subchapterNames, subchapterHandler, activeSubChapter, sectionHandler, activeSection}){
 
     let [open, reverseArrow] = useState(false)
 
     let reverseMenu = () => {reverseArrow(() => (!open))}
 
-    let activeLinkState = (subchapterName) => (
-        subchapterName === activeSubChapter ? "nav-link active" : "nav-link"
+    let activeLinkState = (check)=> 
+        ((name) => (
+            name === check ? "nav-link active" : "nav-link"
+        )
     )
+
+    let activeSubChapterState = activeLinkState(activeSubChapter) + "btn";
+    let activeSectionState = activeLinkState(activeSection);
+
+    let sectionNames = (subchapter) => (subchapter.content.map(obj => obj.name))()
 
     return(
         <div className = "subchapters">
             <ul className = "nav nav-pills flex-column" style = {!open? {display:'none'} : {display: 'flex'}}>
-                {subchapterNames.map(name => (
-                        <li className = "nav-item" id = {subchapterNames.indexOf(name)}>
-                            <a className = {activeLinkState(name)} onClick = {() => subchapterHandler(name)} href="#">
-                                {name}
-                            </a>
-                        </li>
-                    ))}
+                {subchapterNames.map(subchapterName => (
+                    <li className = "card nav-item" id = {subchapterNames.indexOf(subchapterName)}>
+                        <a class= {activeSubChapterState(subchapterName)} data-bs-toggle="collapse" href="#collapseOne"> 
+                            {subchapterName} 
+                        </a>
+                        <ul className = "nav nav-pills flex-column">
+                            <div id="collapseOne" class="collapse" data-bs-parent="#accordion">
+                                {sectionNames.map(sectionName => (
+                                        <li className = "nav-item" id = {sectionNames.indexOf(sectionName)}>
+                                            <a className = {activeSectionState(sectionName)} onClick = {() => {sectionHandler(sectionName); subchapterHandler(subchapterName)}} href="#">
+                                                {sectionName}
+                                            </a>
+                                        </li>
+                                    ))}
+                            </div>
+                        </ul>
+                    </li>
+                ))}
             </ul>
             <ArrowButton iconNames = {{open:"left", closed:"right"}} tooltipName = "Subchapters" menuStateVar = {open} menuStateHandler = {reverseMenu}/>
         </div>
