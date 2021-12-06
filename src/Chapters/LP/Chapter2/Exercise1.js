c21059691bb6879f0bec1aa06afa5093aecc3217
Calin Timofte <calin.timofte@yahoo.com>
2021-12-06 22:06:18 +0100

    feature: start for first exercise

---
+++ src/Chapters/LP/Chapter2/Exercise1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Exercise1(){
+    return(
+        <div>
+            <span>ma</span>
+            <span>joc</span>
+            <span>acasa</span>
+            <span>si</span>
+            <span>invat</span>
+            <span>la</span>
+            <span>scoala</span>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'
+// import App from './App'
+import Exercise1 from './Chapters/LP/Chapter2/Exercise1'
 
 ReactDOM.render(
-  <App/>,
+  <Exercise1/>,
   document.getElementById('root')
 );
