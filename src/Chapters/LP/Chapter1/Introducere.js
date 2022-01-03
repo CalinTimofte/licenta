63a683924c2e0f4a3900bbf17276c949c4b65ae5
Calin Timofte <calin.timofte@yahoo.com>
2022-01-03 22:00:18 +0200

    Starts new direct-pdf-rendering system, working for introduction

--- src/Chapters/LP/Chapter1/Introducere.js
+++ src/Chapters/LP/Chapter1/Introducere.js
@@ -1,77 +1,91 @@
+import { Document, Page, pdfjs } from "react-pdf";
+import React, {useState} from 'react';
+import pdf from "./Introduction.pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+// const LPIntroducere_deprecated = 
+//     <>
+//         <h1>Capitolul 1</h1>
+//         <h2>Introducere</h2>
+//         <p>Dac ̆a aritmetica este s,tiint,a care studiaz ̆a numerele s,i operat,iile cu numere,
+//         logica este s,tiint,a ˆın care obiectul studiului este reprezentat de propozit,ii s,i
+//         de operat,ii cu propozit,ii.
+//         De exemplu, dac ̆a ˆın aritmetic ̆a observ ̆am c ̆a suma a dou ̆a numere pare
+//         este un num ̆ar par, ˆın logic ̆a am putea observa c ̆a disjunct,ia a dou ̆a propozit,ii
+//         adev ̆arate este o propozit,ie adev ̆arat ̆a.
+//         Logica se g ̆ases,te la intersect,ia filosofiei, matematicii s,i informaticii s,i a
+//         cunoscut cea mai mare dezvoltare ˆıncepˆand cu anii 1950, datorit ̆a aplicat,iilor
+//         numeroase ˆın Informatic ̆a.ˆIn acest curs, vom studia la nivel introductiv logica propozit,ional ̆a s,i logica
+//         de ordinul I.
+//         Logica propozit,ional ̆a este extrem de simpl ̆a, dar conceptele pe care le
+//         studiem, metodele pe care le ˆınv ̆at, ̆am s,i problemele pe care le ˆıntˆalnim ˆın
+//         logica propozit,ional ̆a se pot generaliza la alte logici mai complexe. De aseme-
+//         nea, logica propozit,ional ̆a corespunde ˆın mod intim organiz ̆arii interne la nivel
+//         abstract a calculatoarelor, ˆın sensul ˆın care circuitele electronice se pot mod-
+//         ela ca formule din logica propozit,ional ̆a. Logica propozit,ional ̆a are o teorie
+//         bogat ̆a s,i interesant ̆a din punct de vedere matematic (exemple: teorema de
+//         compactitate, teorema de interpolare a lui Craig). Problema satisfiabilit ̆at,ii
+//         pentru logica propozit,ional ̆a are multe aplicat,ii ˆın informatic ̆a. Este deosebit
+//         de important ̆a atˆat din punct de vedere teoretic (fiind problema canonic ̆a NP-
+//         complet ̆a) cˆat s,i practic (cu aplicat,ii ˆın verificarea programelor, ˆın verificarea
+//         circuitelor, ˆın optimizare combinatorial ̆a s,.a.).
+//         Logica de ordinul I este o extensie a logicii propozit,ionale s,i are de aseme-
+//         nea aplicat,ii numeroase ˆın informatic ̆a, dar s,i ˆın matematic ̆a. De exemplu,
+//         toat ̆a matematica pe care at,i ˆınv ̆at,at-o ˆın gimnaziu/liceu se bazeaz ̆a pe o as,a
+//         numit ̆a teorie din logica de ordinul I numit ̆a ZFC (teoria Zermelo–Fraenkel
+//         a mult,imilor, ˆımpreun ̆a cu axioma alegerii – Axiom of Choice). ˆIn Infor-
+//         matic ̆a, aplicat,ii ale logicii de ordinul I apar ˆın domeniul complexit ̆at,ii de-
+//         scriptive, ˆın baze de date relat,ionale, ˆın verificarea automat ̆a a hardware-ului
+//         s,i software-ului, s,.a. De asemenea, multe alte logici (de exemplu, logicile de or-
+//         din superior) au aplicat,ii ˆın teoria limbajelor de programare, ˆın fundamentele
+//         matematicii, teoria tipurilor etc.</p>
+//     </>
 
-export const LPIntroducere = 
-    <>
-        <h1>Capitolul 1</h1>
-        <h2>Introducere</h2>
-        <p>Dac ̆a aritmetica este s,tiint,a care studiaz ̆a numerele s,i operat,iile cu numere,
-        logica este s,tiint,a ˆın care obiectul studiului este reprezentat de propozit,ii s,i
-        de operat,ii cu propozit,ii.
-        De exemplu, dac ̆a ˆın aritmetic ̆a observ ̆am c ̆a suma a dou ̆a numere pare
-        este un num ̆ar par, ˆın logic ̆a am putea observa c ̆a disjunct,ia a dou ̆a propozit,ii
-        adev ̆arate este o propozit,ie adev ̆arat ̆a.
-        Logica se g ̆ases,te la intersect,ia filosofiei, matematicii s,i informaticii s,i a
-        cunoscut cea mai mare dezvoltare ˆıncepˆand cu anii 1950, datorit ̆a aplicat,iilor
-        numeroase ˆın Informatic ̆a.ˆIn acest curs, vom studia la nivel introductiv logica propozit,ional ̆a s,i logica
-        de ordinul I.
-        Logica propozit,ional ̆a este extrem de simpl ̆a, dar conceptele pe care le
-        studiem, metodele pe care le ˆınv ̆at, ̆am s,i problemele pe care le ˆıntˆalnim ˆın
-        logica propozit,ional ̆a se pot generaliza la alte logici mai complexe. De aseme-
-        nea, logica propozit,ional ̆a corespunde ˆın mod intim organiz ̆arii interne la nivel
-        abstract a calculatoarelor, ˆın sensul ˆın care circuitele electronice se pot mod-
-        ela ca formule din logica propozit,ional ̆a. Logica propozit,ional ̆a are o teorie
-        bogat ̆a s,i interesant ̆a din punct de vedere matematic (exemple: teorema de
-        compactitate, teorema de interpolare a lui Craig). Problema satisfiabilit ̆at,ii
-        pentru logica propozit,ional ̆a are multe aplicat,ii ˆın informatic ̆a. Este deosebit
-        de important ̆a atˆat din punct de vedere teoretic (fiind problema canonic ̆a NP-
-        complet ̆a) cˆat s,i practic (cu aplicat,ii ˆın verificarea programelor, ˆın verificarea
-        circuitelor, ˆın optimizare combinatorial ̆a s,.a.).
-        Logica de ordinul I este o extensie a logicii propozit,ionale s,i are de aseme-
-        nea aplicat,ii numeroase ˆın informatic ̆a, dar s,i ˆın matematic ̆a. De exemplu,
-        toat ̆a matematica pe care at,i ˆınv ̆at,at-o ˆın gimnaziu/liceu se bazeaz ̆a pe o as,a
-        numit ̆a teorie din logica de ordinul I numit ̆a ZFC (teoria Zermelo–Fraenkel
-        a mult,imilor, ˆımpreun ̆a cu axioma alegerii – Axiom of Choice). ˆIn Infor-
-        matic ̆a, aplicat,ii ale logicii de ordinul I apar ˆın domeniul complexit ̆at,ii de-
-        scriptive, ˆın baze de date relat,ionale, ˆın verificarea automat ̆a a hardware-ului
-        s,i software-ului, s,.a. De asemenea, multe alte logici (de exemplu, logicile de or-
-        din superior) au aplicat,ii ˆın teoria limbajelor de programare, ˆın fundamentele
-        matematicii, teoria tipurilor etc.</p>
-    </>
+//  const LPIntroduction_deprecated = 
+//     <>
+//         <h1>Chapter 1</h1>
+//         <h2>Introduction</h2>
+//         <p>If Arithmetic is the science that studies numbers and operations with num-
+//             bers, then Logic is the science that studies propositions and operations with
+//             propositions.
+//             For example, if in Arithmetic we notice that the sum of two even numbers
+//             is an even number, then in Logic we could notice that the disjunction of two
+//             true sentences is also true.
+//             Logic sits at the intersection of philosophy, mathematics and computer
+//             science and has experienced its greatest development starting with the 1950s,
+//             because of its numerous applications in Computer Science.
+//             In this course, we will study at an introductory level propositional logic
+//             and first-order logic.
+//             Propositional logic is extremely simple, but the concepts that we study, the
+//             methods that we learn and the issues that we face in propositional logic gener-
+//             alize to other more complex logics. Moreover, propositional logic corresponds
+//             intimally to the internal organization of computers at an abstract level, in
+//             the sense that electronic circuits can be modeled as formulae in propositional
+//             logic. Propositional logic has a rich and mathematically interesting theory
+//             (examples: compactness theorem, Craig’s interpolation theorem). The sat-
+//             isfiability problem for propositional logic has many applications in computer
+//             science. It is especially important both from a theoretical viewpoint (being
+//             the canonical NP-complete problem) and from a practical viewpoint as well
+//             (with applications in program verification, circuit verification, combinatorial
+//             optimization, and others).
+//             First-order logic is an extension of propositional logic and also has numer-
+//             ous application in computer science, but also in mathematics. For example,
+//             all of the math that you have studied in highschool is based on a so-called first-
+//             order logic theory called ZFC (the Zermelo–Fraenkel set theory, together with
+//             the Axiom of Choice). In Computer Science, applications of first-order logic
+//             appear in the fields of descriptive complexity, relational databases, software
+//             and hardware verification, and others. Additionally, several other logics (for
+//             example, higher-order logics) have applications in programming languages,
+//             the fundamentals of mathematics, type theory, etc.</p>
+//     </>
 
-export const LPIntroduction = 
-    <>
-        <h1>Chapter 1</h1>
-        <h2>Introduction</h2>
-        <p>If Arithmetic is the science that studies numbers and operations with num-
-            bers, then Logic is the science that studies propositions and operations with
-            propositions.
-            For example, if in Arithmetic we notice that the sum of two even numbers
-            is an even number, then in Logic we could notice that the disjunction of two
-            true sentences is also true.
-            Logic sits at the intersection of philosophy, mathematics and computer
-            science and has experienced its greatest development starting with the 1950s,
-            because of its numerous applications in Computer Science.
-            In this course, we will study at an introductory level propositional logic
-            and first-order logic.
-            Propositional logic is extremely simple, but the concepts that we study, the
-            methods that we learn and the issues that we face in propositional logic gener-
-            alize to other more complex logics. Moreover, propositional logic corresponds
-            intimally to the internal organization of computers at an abstract level, in
-            the sense that electronic circuits can be modeled as formulae in propositional
-            logic. Propositional logic has a rich and mathematically interesting theory
-            (examples: compactness theorem, Craig’s interpolation theorem). The sat-
-            isfiability problem for propositional logic has many applications in computer
-            science. It is especially important both from a theoretical viewpoint (being
-            the canonical NP-complete problem) and from a practical viewpoint as well
-            (with applications in program verification, circuit verification, combinatorial
-            optimization, and others).
-            First-order logic is an extension of propositional logic and also has numer-
-            ous application in computer science, but also in mathematics. For example,
-            all of the math that you have studied in highschool is based on a so-called first-
-            order logic theory called ZFC (the Zermelo–Fraenkel set theory, together with
-            the Axiom of Choice). In Computer Science, applications of first-order logic
-            appear in the fields of descriptive complexity, relational databases, software
-            and hardware verification, and others. Additionally, several other logics (for
-            example, higher-order logics) have applications in programming languages,
-            the fundamentals of mathematics, type theory, etc.</p>
-    </>
+const ImportantDocument = () => {
+    return (
+        <Document file= {pdf}>
+            <Page pageNumber = {1} scale={2.0}/>
+            <Page pageNumber = {2} scale={2.0}/>
+        </Document>
+    );
+  }
+
+export const LPIntroduction = <ImportantDocument/> ;
