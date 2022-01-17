f1dc87719670c9a5a31d5ec601e5f6221fdbdf2a
Calin Timofte <calin.timofte@yahoo.com>
2022-01-17 21:43:49 +0100

    feature: Adds simple expression parsing (extendable) with the shunting yard alghoritm

--- src/Reusables/LogicCheking/LP/Parser.js
+++ src/Reusables/LogicCheking/LP/Parser.js
@@ -4,11 +4,37 @@ import computeConjuncton from "./Conjunction";
 
 function checkFormatting(input){
     // Does not take not into account yet
-    debugger
-    if(typeof input[0] === String || typeof input[input.length] === String)
+    if(typeof input[0] === "string" || typeof input[input.length] === "string")
         throw("Malformed input");
     for(let i = 0; i < input.length - 1; i++){
         if(typeof(input[i]) === typeof(input[i+1]))
             throw ("Malformed input");
     }
+}
+
+function applyShuntingYard(input){
+    checkFormatting(input);
+    let priorityOfOperations = ["and"];
+    let queue = [];
+    let stack = [];
+    for(let i = 0; i < input.length; i++){
+        if(!priorityOfOperations.includes(input[i]))
+            queue.push(input[i]);
+        else{
+            if(input.length === 0)
+                stack.unshift(input[i]);
+            else{
+                if(priorityOfOperations.indexOf(input[i]) > priorityOfOperations.indexOf(stack[0]))
+                    stack.unshift(input[i]);
+                else{
+                    while(priorityOfOperations.indexOf(input[i]) <= priorityOfOperations.indexOf(stack[0]))
+                        queue.push(stack.shift());
+                        stack.unshift(input[i]);
+                }
+            }
+        }
+    }
+    while(stack.length > 0)
+        queue.push(stack.shift());
+    return queue
 }
