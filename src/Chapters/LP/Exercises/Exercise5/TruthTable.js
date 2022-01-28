d5428b8c3b9cd4a96d4b06c42040a055e35c1147
Calin Timofte <calin.timofte@yahoo.com>
2022-01-28 01:59:29 +0100

    feature: Finishes exercise 5

--- src/Chapters/LP/Exercises/Exercise5/TruthTable.js
+++ src/Chapters/LP/Exercises/Exercise5/TruthTable.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function TruthTable(){
+
+    let [answerTruthValues, changeAnswerTruthValues] = useState(["true", "true", "true", "true"]);
+    let [completedness, changeCompleteness] = useState("unattempted");
+
+    let selectFactory = (answerIndex) => (
+        <select class="form-select" id="truth-values" name="truth-values"
+            onChange={(e) => {
+                let optionIndex = e.target.selectedIndex;
+                changeAnswerTruthValues(answerTruthValues => {
+                    let newAnswerTruthValues = [...answerTruthValues];
+                    newAnswerTruthValues[answerIndex] = e.target.options[optionIndex].value;
+                    return newAnswerTruthValues;
+                });
+            }}
+        >
+            <option value="true">true</option>
+            <option value="false">false</option>
+        </select> 
+    );
+
+    let checkAnswer = () => {
+        if(answerTruthValues[0] === "false")
+            if((answerTruthValues[1] === "true" && answerTruthValues[2] === "true") && answerTruthValues[3] === "true")
+                changeCompleteness("complete");
+            else
+                changeCompleteness("incomplete");
+        else
+            changeCompleteness("incomplete");
+
+    }
+
     return(
         <div>
             <table class="table">
@@ -16,48 +47,38 @@ export default function TruthTable(){
                         <td>false</td>
                         <td>false</td>
                         <td>
-                            <label for="truth-values">Choose truth value:</label>
-                            <select class="form-select" id="truth-values" name="truth-values">
-                            <option value="true">true</option>
-                            <option value="false">false</option>
-                            </select> 
+                            {selectFactory(0)}
                         </td>
                     </tr>
                     <tr>
                         <td>false</td>
                         <td>true</td>
                         <td>
-                            <label for="truth-values">Choose truth value:</label>
-                            <select class="form-select" id="truth-values" name="truth-values">
-                            <option value="true">true</option>
-                            <option value="false">false</option>
-                            </select> 
+                            {selectFactory(1)}
                         </td>
                     </tr>
                     <tr>
                         <td>true</td>
                         <td>false</td>
                         <td>
-                            <label for="truth-values">Choose truth value:</label>
-                            <select class="form-select" id="truth-values" name="truth-values">
-                            <option value="true">true</option>
-                            <option value="false">false</option>
-                            </select> 
+                            {selectFactory(2)}
                         </td>
                     </tr>
                     <tr>
                         <td>true</td>
                         <td>true</td>
                         <td>
-                            <label for="truth-values">Choose truth value:</label>
-                            <select class="form-select" id="truth-values" name="truth-values">
-                            <option value="true">true</option>
-                            <option value="false">false</option>
-                            </select> 
+                            {selectFactory(3)} 
                         </td>
                     </tr>
                 </tbody>
             </table>
+
+            <button type="button" className="btn btn-outline-dark" onClick={checkAnswer}>Done</button>
+            <span>
+                {completedness === "complete"? <p style = {{color: "green"}}>Congratulations!</p> :
+                    completedness === "incomplete"? <p style = {{color: "red"}}>Try again!</p> : <p></p>}
+            </span>
         </div>
     )
 }
