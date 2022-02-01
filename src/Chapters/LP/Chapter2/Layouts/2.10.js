ab6a3795688b61cb3533389e06b5ee9a3033836f
Calin Timofte <calin.timofte@yahoo.com>
2022-02-01 22:09:07 +0100

    feature: Adds Exercise 9 and 10, finished chapter 2

--- src/Chapters/LP/Chapter2/Layouts/2.10.js
+++ src/Chapters/LP/Chapter2/Layouts/2.10.js
@@ -1,6 +1,7 @@
 import { Document, Page, pdfjs } from "react-pdf";
 import React from 'react';
 import pdf from "../Chapter2.pdf";
+import Exercise9 from "../../Exercises/Exercise9/Exercise9.js"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfLayout = () => {
@@ -9,6 +10,9 @@ const PdfLayout = () => {
             <Page pageNumber = {24} width={window.screen.availWidth * 0.5}/>
             <Page pageNumber = {25} width={window.screen.availWidth * 0.5}/>
             <Page pageNumber = {26} width={window.screen.availWidth * 0.5}/>
+            <br/>
+                <Exercise9/>
+            <br/>
             <Page pageNumber = {3} width={window.screen.availWidth * 0.5}/>
         </Document>
     );
---
+++ src/Chapters/LP/Exercises/Exercise9/Exercise9.js
@@ -0,0 +1,81 @@
+import React from "react";
+import ValueTable from "../Exercise5/ValueTable.js";
+
+export default function Exercise9(){
+
+    return(
+        <div className="exercise">
+            <div className="card">
+                <div className="card-body bg-light">
+                    <ValueTable
+                        tableValues={[
+                            ["Sentence", "Value"], 
+                            ["You own a laptop computer.", "select"],
+                            ["Snow is white.", "select"],
+                            ["Snow is not white.", "select"],
+                            ["My father goes to work and I go to school.", "select"],
+                            ["It is raining outside, but I have an umbrella.", "select"],
+                            ["Either it will rain tomorrow, or it won’t rain.", "select"],
+                            ["If I get a passing grade in Logic, I will celebrate.", "select"],
+                            ["2 + 2 = 4. ( Two plus two is four.)", "select"],
+                            ["Red and Black. (not a statement)", "select"],
+                            ["π. (not a statement)", "select"],
+                            ["Is it raining? (question)", "select"],
+                            ["Let’s go fishing! (imperative)", "select"],
+                            ["x is greater than 7. (cannot tell unless I know who x is)", "select"],
+                            ["This sentence is false.", "select"],
+                        ]}
+                        checkAnswer={
+                            (answerTruthValues, changeCompleteness) => {
+                                if(answerTruthValues[0] === "atomic proposition")
+                                    if(answerTruthValues[1] === "atomic proposition")
+                                        if(answerTruthValues[2] === "molecular proposition - negation")
+                                            if(answerTruthValues[3] === "molecular proposition - conjunction")
+                                                if(answerTruthValues[4] === "molecular proposition - conjunction")
+                                                    if(answerTruthValues[5] === "molecular proposition - disjunction")
+                                                        if(answerTruthValues[6] === "molecular proposition - implication")
+                                                            if(answerTruthValues[7] === "atomic proposition")
+                                                                if(answerTruthValues[8] === "molecular proposition - conjunction")
+                                                                    if(answerTruthValues[9] === "atomic proposition")
+                                                                        if(answerTruthValues[10] === "not proposition")
+                                                                            if(answerTruthValues[11] === "not proposition")
+                                                                                if(answerTruthValues[12] === "atomic proposition")
+                                                                                    if(answerTruthValues[13] === "atomic proposition")
+                                                                                        changeCompleteness("complete");
+                                                                                    else
+                                                                                        changeCompleteness("incomplete");
+                                                                                else
+                                                                                    changeCompleteness("incomplete");
+                                                                            else
+                                                                                changeCompleteness("incomplete");
+                                                                        else
+                                                                            changeCompleteness("incomplete");
+                                                                    else
+                                                                        changeCompleteness("incomplete");
+                                                                else
+                                                                    changeCompleteness("incomplete");
+                                                            else
+                                                                changeCompleteness("incomplete");
+                                                        else
+                                                            changeCompleteness("incomplete");
+                                                    else
+                                                        changeCompleteness("incomplete");
+                                                else
+                                                    changeCompleteness("incomplete");
+                                            else
+                                                changeCompleteness("incomplete");
+                                        else
+                                            changeCompleteness("incomplete");
+                                    else
+                                        changeCompleteness("incomplete");
+                                else
+                                    changeCompleteness("incomplete");
+                        
+                        }}
+                        selectValues = {["not proposition", "atomic proposition", "molecular proposition - conjunction", "molecular proposition - disjunction", "molecular proposition - negation", "molecular proposition - implication", "molecular proposition - equivalence"]}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
