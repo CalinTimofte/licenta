4b324613bff924acd86bbd5d5a962f4c97f709cc
Calin Timofte <calin.timofte@yahoo.com>
2022-02-07 22:55:57 +0200

    feature: Adds FileUploader component, localstorage not working yet

--- src/Chapters/LP/Chapter3/Layouts/3.9.js
+++ src/Chapters/LP/Chapter3/Layouts/3.9.js
@@ -2,8 +2,9 @@ import { Document, Page, pdfjs } from "react-pdf";
 import React from 'react';
 import pdf from "../Chapter3.pdf";
 import Exercise14 from "../../Exercises/Exercise14/Exercise14";
-import Exercise16 from "../../Exercises/Exercise16/Exercise16.js"
-import Exercise17 from "../../Exercises/Exercise17/Exercise17.js"
+import Exercise16 from "../../Exercises/Exercise16/Exercise16.js";
+import Exercise15 from "../../Exercises/Exercise15/Exercise15.js";
+import Exercise17 from "../../Exercises/Exercise17/Exercise17.js";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfLayout = () => {
@@ -14,6 +15,9 @@ const PdfLayout = () => {
                 <Exercise14/>
             <br/>
             <Page pageNumber = {19} width={window.screen.availWidth * 0.5}/>
+            <br/>
+                <Exercise15/>
+            <br/>
             <Page pageNumber = {21} width={window.screen.availWidth * 0.5}/>
             <br/>
                 <Exercise16/>
---
+++ src/Chapters/LP/Exercises/Exercise15/Exercise15.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FileUploadAndView from "./FileUploadAndView";
+
+export default function Exercise15(){
+
+    return(
+        <div className="exercise">
+            <div className="card">
+                <div className="card-body bg-light">
+                    <FileUploadAndView/>
+                </div>
+            </div>
+        </div>
+    )
+}
