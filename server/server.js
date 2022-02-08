e7d03c42c048d747efba9b0a7b4dc43d2c8d23b1
Calin Timofte <calin.timofte@yahoo.com>
2022-02-08 20:17:04 +0200

    db: Connects to the db

--- server/server.js
+++ server/server.js
@@ -1,13 +1,38 @@
+require("dotenv").config({path:__dirname + '/.env'})
 const express = require('express');
-const path = __dirname + '/app/views';
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose")
+
 const app = express();
-app.use(express.static(path));
-const port = 3001;
 
-app.get('/', (req, res) => {
-    res.sendFile(path + "index.html")
-})
+// connect to DB
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {console.log("Connected to the database!");})
+    .catch(err => {
+        console.log("Cannot conenct to the database!", err);
+        process.exit();
+    })
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
+let corsOptions = {
+    origin: "http://localhost:3001"
+};
+
+app.use(cors(corsOptions));
+
+// parse request of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json("Welcome to thesis server.");
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
