7752a958ab4fb3c62fc3ab13a83d0c34ce448dd4
Calin Timofte <calin.timofte@yahoo.com>
2022-02-09 21:24:50 +0200

    feature: Adds user db and implements test features

--- client/src/DBTest.js
+++ client/src/DBTest.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 
 export default function DBTest(){
+    let [userName, changeUserName] = useState("");
+    let [oldUserName, changeOldUserName] = useState("");
+    let [newUserName, changeNewUserName] = useState("");
+
+    let [password, changePassword] = useState("");
+    let [oldPassword, changeOldPassword] = useState("");
+    let [newPassword, changeNewPassword] = useState("");
+
+    let [updates, changeUpdates] = useState(0);
+    let [johnsList, changeJohnsList] = useState([]);
+
+    let inputChangeHandlerFactory = (stateChangeFunc) => ((event) => {stateChangeFunc(event.target.value)});
+    let handleUserNameChange = inputChangeHandlerFactory(changeUserName);
+    let handleOldUserNameChange = inputChangeHandlerFactory(changeOldUserName);
+    let handleNewUserNameChange = inputChangeHandlerFactory(changeNewUserName);
+    let handlePasswordChange = inputChangeHandlerFactory(changePassword);
+    let handleOldPasswordChange = inputChangeHandlerFactory(changeOldPassword);
+    let handleNewPasswordChange = inputChangeHandlerFactory(changeNewPassword);
+
+    let incrementUpdates = () => {changeUpdates(updates => updates+1)};
+    let overWriteJohnsList = (newList) => {changeJohnsList(newList)};
+
+    useEffect(() => {
+        setTimeout(() => {
+            http.get("/getAllUsers")
+                .then((response) => {overWriteJohnsList(response.data)})
+                .catch((error) => {console.log(error)})}, 100)
+    }, [updates])
 
     let http = axios.create({
         baseURL: "http://localhost:3001",
@@ -10,13 +38,98 @@ export default function DBTest(){
         }
     })
 
-    let createJohn = () => {
-        http.get("/testUser")
+    let createHandlerFactory = (route) => (() => {
+        http.post(route, {
+            userName: userName,
+            password: password
+        }).then(() => {incrementUpdates()})
+    })
+    
+    let createStudent = createHandlerFactory("/createStudent");
+    let createProfessor = createHandlerFactory("/createProfessor");
+    let createAdmin = createHandlerFactory("/createAdmin");
+
+    let deleteAllUsers = () => {
+        http.get("/deleteTestUsers").then(() => {incrementUpdates()})
+    }
+
+    let searchUser = () => {
+        http.post("/findUser", {
+            userName: oldUserName
+        })
+    }
+
+    let searchUSerSecurely = () => {
+        http.post("/findUserSecurely", {
+            userName: oldUserName,
+            password: oldPassword
+        })
+    }
+
+    let update = () => {
+        http.post("/findAndUpdateUser", {
+            oldUserName: oldUserName,
+            newUserName: newUserName,
+            newPassword: newPassword
+        }).then(() => {incrementUpdates()})
+    }
+
+    let updateSecure = () => {
+        http.post("/findAndUpdateUserSecurely", {
+            oldUserName: oldUserName,
+            newUserName: newUserName,
+            oldPassword: oldPassword,
+            newPassword: newPassword
+        }).then(() => {incrementUpdates()})
     }
 
     return(
-        <div>
-            <button className="btn btn-outline-dark" onClick={createJohn}>Add John Doe</button>
+        <div className="testing">
+            <div>
+                <label>New User details:</label>
+                <input type="text" placeholder = "username" onChange = {handleUserNameChange}></input>
+                <input type="text" placeholder = "password" onChange= {handlePasswordChange}></input>
+            </div>
+            <button className="btn btn-outline-dark" onClick={createStudent}>Spawn Student</button>
+            <button className="btn btn-outline-dark" onClick={createProfessor}>Spawn Professor</button>
+            <button className="btn btn-outline-dark" onClick={createAdmin}>Spawn Admin</button>
+            <button className="btn btn-outline-dark" onClick={deleteAllUsers}>Delete all users</button>
+            <div>
+                <label>Modify Specific John:</label>
+                <input type="text" placeholder = "John Doe old username" onChange = {handleOldUserNameChange}></input>
+                <input type="text" placeholder = "John Doe new username" onChange= {handleNewUserNameChange}></input>
+                <input type="text" placeholder = "John Doe old password" onChange= {handleOldPasswordChange}></input>
+                <input type="text" placeholder = "John Doe new password" onChange= {handleNewPasswordChange}></input>
+            </div>
+            <button className="btn btn-outline-dark" onClick={searchUser}>Look for user</button>
+            <button className="btn btn-outline-dark" onClick={searchUSerSecurely}>Look for user (with password)</button>
+            <button className="btn btn-outline-dark" onClick={update}>Update</button>
+            <button className="btn btn-outline-dark" onClick={updateSecure}>Update after checking password</button>
+            <div>
+                <p>Johns:</p>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope = "col">id</th>
+                            <th scope="col">User Name</th>
+                            <th scope="col">Password</th>
+                            <th scope="col">Priviledge</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {johnsList.map((john, index) => (
+                            <tr key = {index}>
+                                <th scope="row">{index}</th>
+                                <td>{john._id}</td>
+                                <td>{john.userName}</td>
+                                <td>{john.password}</td>
+                                <td>{john.priviledge}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    </table>
+            </div>
         </div>
     )
 }
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     })
 
 let corsOptions = {
-    origin: "http://localhost:3001"
+    origin: ["http://localhost:3001", "http://localhost:3000"]
 };
 
 app.use(cors(corsOptions));
@@ -34,26 +34,134 @@ const userSchema = new Schema({
     priviledge: {type: Number, required: true}
 });
 
+const classRoomSchema = new Schema({
+    classRoomName: {type: String, required: true},
+    proffesorID: Schema.Types.ObjectId,
+    studentsIDs: [Schema.Types.ObjectID]
+})
+
 let User = mongoose.model("User", userSchema);
 
-const createAndSaveTestUser = (done) => {
-    const doe = new User({userName: 'JohnDoe', password: 'johndoe1', priviledge: 3});
+const createAndSaveUser = (userName, password, priviledge, done) => {
+    const doe = new User({userName: userName, password: password, priviledge: priviledge});
     doe.save((err, data) => {
         if (err) return console.error(err);
-        done(data, data);
+        done(null, data);
     });
 };
+
+const deleteAllTestUsers = (done) => {
+    User.deleteMany(null,(err, data) => {
+        if (err) return console.error(err);
+        done(null, data);
+    });
+};
+
+const getAllUsers = (done) => {
+    User.find({},(err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    });
+  };
+
+const findUserByUserName = (userName, done) => {
+    User.find({userName: userName},(err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    });
+  };
+
+  const findUserByUserNameAndPassword = (userName, password, done) => {
+    User.find({userName: userName, password: password},(err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    });
+  };
+
+const findUserByUserNameAndUpdate = (oldUserName, newUserName, newPassword, done) => {
+    findUserByUserName(oldUserName, (err, data) => {
+        User.findByIdAndUpdate(data[0].id, {userName: newUserName, password: newPassword}, (err, data)=> {
+            if (err) return console.err(err);
+            done(null, data);
+        });
+    });
+}
+
+const findUserByUserNameAndPasswordAndUpdate = (oldUserName, newUserName, oldPassword, newPassword, done) => {
+    findUserByUserNameAndPassword(oldUserName, oldPassword, (err, data) => {
+        User.findByIdAndUpdate(data[0].id, {userName: newUserName, password: newPassword}, (err, data)=> {
+            if (err) return console.err(err);
+            done(null, data);
+        });
+    });
+}
 
 // simple route
 app.get("/", (req, res) => {
     res.json("Welcome to thesis server.");
 });
 
-app.get("/testUser", (req, res) => {
-    createAndSaveTestUser(done => {
-        console.log(done);
+app.post("/createStudent", (req, res) => {
+    createAndSaveUser(req.body.userName, req.body.password, 1, (err, data) => {
+        console.log(data);
     });
-    res.json("You created John Doe.");
+    res.json("You created a special John Doe.");
+});
+
+app.post("/createProfessor", (req, res) => {
+    createAndSaveUser(req.body.userName, req.body.password, 2, (err, data) => {
+        console.log(data);
+    });
+    res.json("You created a special John Doe.");
+});
+
+app.post("/createAdmin", (req, res) => {
+    createAndSaveUser(req.body.userName, req.body.password, 3, (err, data) => {
+        console.log(data);
+    });
+    res.json("You created a special John Doe.");
+});
+
+app.get("/deleteTestUsers", (req, res) => {
+    deleteAllTestUsers((err, data) => {
+        console.log(data);
+    });
+    res.json("You killed all John Does :(");
+});
+
+app.post("/findUser", (req, res) => {
+    findUserByUserName(req.body.userName, (err, data) => {
+        console.log(data);
+    });
+    res.json("You created a special John Doe.");
+});
+
+app.post("/findUserSecurely", (req, res) => {
+    findUserByUserNameAndPassword(req.body.userName, req.body.password, (err, data) => {
+        console.log(data);
+    });
+    res.json("You created a special John Doe.");
+});
+
+app.post("/findAndUpdateUser", (req, res) => {
+    findUserByUserNameAndUpdate(req.body.oldUserName, req.body.newUserName, req.body.newPassword, (err, data) => {
+        console.log(data);
+    });
+    res.json("You modified Bon Joe");
+});
+
+app.post("/findAndUpdateUserSecurely", (req, res) => {
+    findUserByUserNameAndPasswordAndUpdate(req.body.oldUserName, req.body.newUserName, req.body.oldPassword, req.body.newPassword, (err, data) => {
+        console.log(data);
+    });
+    res.json("You modified Bon Jovi");
+});
+
+app.get("/getAllUsers", (req, res) => {
+    getAllUsers((err, data) => {
+        console.log(data);
+        res.json(data);
+    });
 });
 
 // set port, listen for requests
