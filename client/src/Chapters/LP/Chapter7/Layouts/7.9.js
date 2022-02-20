e64ee32e8ea2e2852136a03f8217f5f45ed95430
Calin Timofte <calin.timofte@yahoo.com>
2022-02-20 03:21:29 +0200

    bugfix: removes rectangles from chapter 9.4

--- client/src/Chapters/LP/Chapter7/Layouts/7.9.js
+++ client/src/Chapters/LP/Chapter7/Layouts/7.9.js
@@ -1,19 +1,19 @@
 import { Document, Page, pdfjs } from "react-pdf";
 import React from 'react';
-import pdf from "../Chapter7.pdf";
+import pdf from "../notes_pl_no_links-63_cropped.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfLayout = () => {
     return (
         <Document file= {pdf}>
-            <Page pageNumber = {31} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {32} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {33} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {34} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {35} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {37} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {36} width={window.screen.availWidth * 0.5}/>
             <Page pageNumber = {2} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {3} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {4} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {5} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {6} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {7} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {8} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {1} width={window.screen.availWidth * 0.5}/>
         </Document>
     );
   }
--- client/src/Chapters/LP/Chapter9/Layouts/9.4.js
+++ client/src/Chapters/LP/Chapter9/Layouts/9.4.js
@@ -1,17 +1,17 @@
 import { Document, Page, pdfjs } from "react-pdf";
 import React from 'react';
-import pdf from "../Chapter9.pdf";
+import pdf from "../notes_pl_no_links77-79_cropped.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfLayout = () => {
     return (
         <Document file= {pdf}>
-            <Page pageNumber = {14} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {15} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {16} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {17} width={window.screen.availWidth * 0.5}/>
-            <Page pageNumber = {18} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {2} width={window.screen.availWidth * 0.5}/>
             <Page pageNumber = {3} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {4} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {5} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {6} width={window.screen.availWidth * 0.5}/>
+            <Page pageNumber = {1} width={window.screen.availWidth * 0.5}/>
         </Document>
     );
   }
